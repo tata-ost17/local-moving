dd0b0f4d26a737040e464356216b05375bd8bb8a
tata-ost17 <tata.ost17@gmail.com>
2020-06-22 22:08:59 +0300

    added section bonus

--- gulpfile.js
+++ gulpfile.js
@@ -95,6 +95,9 @@ gulp.task("style", function () {
 			"node_modules/normalize.css/normalize.css",
 			"node_modules/jquery-form-styler/dist/jquery.formstyler.css",
 			"node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css",
+			"node_modules/flipclock/dist/flipclock.css",
+			"node_modules/animate.css/animate.css",
+			
 		])
 		.pipe(sourcemaps.init())
 		.pipe(concat("libs.min.css")) //склеиваем их в один файл с указанным именем
@@ -111,6 +114,7 @@ gulp.task("script", function () {
 			//тут подключаем разные js в общую библиотеку. Отключите то, что вам не нужно.
 			"node_modules/jquery/dist/jquery.js",
 			"node_modules/jquery-form-styler/dist/jquery.formstyler.js",
+			"node_modules/flipclock/dist/flipclock.js",
 		])
 		.pipe(size())
 		.pipe(sourcemaps.init())
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,22 @@
-$(function(){
+$(function () {
 
-   
+
    $('input[type="file"],input[type="checkbox"],input[type="radio"],select').styler();
+
+   $(document).ready(function () {
+      var clock;
+      clock = $('.clock').FlipClock({
+         clockFace: 'DailyCounter',
+         autoStart: false,
+         callbacks: {
+            stop: function () {
+               $('.message').html('The clock has stopped!')
+            }
+         }
+      });
+      clock.setTime(220880); //Устанавливаем нужное время в секундах
+      clock.setCountdown(true); //Устанавливаем отсчет времени назад
+      clock.start(); //Запускаем отсчет
+   });
+
 });
